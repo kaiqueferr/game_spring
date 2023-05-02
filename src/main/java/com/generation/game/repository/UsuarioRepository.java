081f509db00197842d99487016e19fbfbc24783d
Kaique <kaique.ferr@gmail.com>
2023-05-02 00:12:05 -0300

    add security

---
+++ src/main/java/com/generation/game/repository/UsuarioRepository.java
@@ -0,0 +1,10 @@
+package com.generation.game.repository;
+
+import java.util.Optional;
+import org.springframework.data.jpa.repository.JpaRepository;
+import com.generation.game.model.Usuario;
+
+public interface UsuarioRepository extends JpaRepository<Usuario, Long>{
+
+	Optional<Usuario> findByUsuario(String usuario);
+}
